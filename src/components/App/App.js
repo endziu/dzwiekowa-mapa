2647a2657f01d51db4e1488b106644dd0f9dd739
endziu <koper.andrzej@gmail.com>
2017-02-25 23:00:27 +0100

    feature: show current rec on the list

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import sounds from '../../assets/tracks.json'
+import { asNumber } from '../../helpers/index.js'
 
 // const sounds = data.sort(() => 0.5 - Math.random())
 
 class App extends Component {
+
   render () {
     return (
       <div className='App flex f6 black-80'>
@@ -16,15 +18,16 @@ class App extends Component {
             <input className='input-reset h2' id='search' placeholder='🔎 Szukaj dźwięków...' type='text' />
           </form>
           <ul className='App-Lista list mt2 lh-copy overflow-auto'>
-            { sounds.map((item, index) => <Link
-              className='link'
-              key={index}
-              to={`/rec/${item.id}`}
+            { sounds.map((item, index) =>
+              <Link
+                className='link'
+                key={index}
+                to={`/rec/${item.id}`}
               >
-              <li className='black-80 hover-near-white bg-animate hover-bg-black-80 truncate'>
-                <span>{item.title}</span>
-              </li>
-            </Link>) }
+                <li className={asNumber(this.props.params.id) === asNumber(item.id) ? 'white bg-black-80 truncate' : 'black-80 hover-near-white bg-animate hover-bg-black-80 truncate'}>
+                  <span>{item.title}</span>
+                </li>
+              </Link>) }
           </ul>
         </div>
       </div>
@@ -33,3 +36,4 @@ class App extends Component {
 }
 
 export default App
+
