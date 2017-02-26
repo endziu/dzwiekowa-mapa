5d0bf267d6798c674651abd29d393829185fffa1
endziu <koper.andrzej@gmail.com>
2017-02-26 22:08:13 +0100

    fix width in welcome component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -35,7 +35,7 @@ class Layout extends Component {
     return (
       <div className='App flex flex-row justify-end f6 black-80'>
 
-        <div className='App-main vh-100'>
+        <div className='App-main vw-100 vh-100'>
 
           {this.props.children}
 
