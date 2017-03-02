e89135672cf77067bd62ea173171bac4a10c4c1d
endziu <koper.andrzej@gmail.com>
2017-03-02 03:49:14 +0100

    still works

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,17 +3,19 @@ import { takeFirst } from '../helpers/'
 
 const Info = ({selectedSound}) => <div className='flex flex-row justify-start items-start pb2 dn-sh'>
   <img
-    className='pt2'
+    className='pt2 dontScale'
     src={selectedSound.artwork_url || selectedSound.userPic}
     alt='artwork'
   />
-  <p className='measure pt2 pl2'>
+  <div className='overflow-auto'>
+  <p className='measure-wide pt2 pl2 infoHeight'>
     { takeFirst(
           selectedSound.description
             .split('\n')
             .filter(s => s.includes('Opis'))
       ).substring(5) }
   </p>
+  </div>
 </div>
 
 export default Info
