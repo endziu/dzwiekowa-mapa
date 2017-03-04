b2ef6710f5e0028656dcf11ba5764b558a496a01
endziu <koper.andrzej@gmail.com>
2017-03-04 22:39:20 +0100

    make new component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,21 +1,7 @@
 import React from 'react'
-import { takeFirst } from '../helpers/'
 
-const Info = ({ selectedSound }) => (
-  <div className='flex flex-row justify-start items-start dn-sh mt1 bg-near-white'>
-    <img
-      className='w3 dontScale'
-      src={selectedSound.artwork_url || selectedSound.userPic}
-      alt='artwork'
-    />
-    <div className='overflow-auto pl2'>
-      <p className='measure-wide infoHeight'>
-        {takeFirst(
-          selectedSound.description.split('\n').filter(s => s.includes('Opis'))
-        ).substring(5)}
-      </p>
-    </div>
-  </div>
-)
+const Info = () => <div>
+  <p>szipupi info info szipapi</p>
+</div>
 
 export default Info
