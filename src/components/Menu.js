344c68b8c42b0d1a4d507f1e15af98b0cb20677f
endziu <koper.andrzej@gmail.com>
2017-03-04 02:17:30 +0100

    switch order

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,9 +13,7 @@ const Menu = () => (
           <path d='M0 8 L16 8' />
           <path d='M0 13.5 L16 13.5' />
         </svg>
-
       </Link>
-
       <Link
         to={'/'}
         title={'Home'}
@@ -24,9 +22,7 @@ const Menu = () => (
           <title>Home icon</title>
           <path d='M16 0 L32 16 L28 16 L28 30 L20 30 L20 20 L12 20 L12 30 L4 30 L4 16 L0 16 Z' />
         </svg>
-
       </Link>
-
       <Link
         to={'/'}
         title={'Pics'}
@@ -34,9 +30,15 @@ const Menu = () => (
         <svg className='w1' viewBox='0 0 24 24' fill='currentcolor'>
           <path d='M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z' />
         </svg>
-
       </Link>
-
+      <Link
+        to={'/'}
+        title={'Info'}
+        className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
+        <svg className='w1' viewBox='0 0 24 24' fill='currentcolor'>
+          <path d='M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z' />
+        </svg>
+      </Link>
       <Link
         to={'/'}
         title={'Add Sound'}
@@ -45,19 +47,7 @@ const Menu = () => (
           <path d='M0 8 L16 8' />
           <path d='M8 0 L8 16' />
         </svg>
-
       </Link>
-
-      <Link
-        to={'/'}
-        title={'Info'}
-        className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
-        <svg className='w1' viewBox='0 0 24 24' fill='currentcolor'>
-          <path d="M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z" />
-        </svg>
-
-      </Link>
-
     </nav>
   </div>
 )
