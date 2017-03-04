d2bd4a92d38f643e3a665a788e1e5aab17443d35
endziu <koper.andrzej@gmail.com>
2017-03-05 00:03:56 +0100

    refactor

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,24 +2,22 @@ import React, { Component } from 'react'
 import List from './List.js'
 import Menu from './Menu.js'
 import Sound from './Sound.js'
-import data from '../assets/tracks.json'
-import { getSoundById, randomizeList } from '../helpers/'
+import sounds from '../assets/tracks.json'
+import { getSoundById/*, randomizeList*/ } from '../helpers/'
 
-const sounds = randomizeList(data)
+//const sounds = randomizeList(data)
 
 class Layout extends Component {
   render () {
-    const { mapa, info } = this.props
+    const currentSound = getSoundById(this.props.params.id, sounds)
     return (
       <div className='App flex-auto flex-ns flex-row-ns justify-end f6 black-80 bg-white'>
-        { this.props.params.id ? <Menu /> : null }
+        { this.props.params.id ? <Menu currentId={this.props.params.id} /> : null }
         <div className='App-main w-100 vh-40 vh-100-ns fadeIn animated ph1 pt1'>
           {this.props.children}
-          {mapa ? { mapa } : null}
-          {info ? { info } : null}
         </div>
         <div className='flex flex-column vh-60 vh-100-ns w-100 mw6-ns ph1 bl-m bl-l fadeIn animated'>
-          <Sound currentSound={getSoundById(this.props.params.id, sounds)} />
+          <Sound currentSound={currentSound} />
           <List currentId={this.props.params.id} sounds={sounds} />
         </div>
       </div>
