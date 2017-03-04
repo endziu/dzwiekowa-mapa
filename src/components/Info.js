8b5a665b8ab18f2915d0624386d19c0873096b00
endziu <koper.andrzej@gmail.com>
2017-03-04 02:08:16 +0100

    fix padding

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { takeFirst } from '../helpers/'
 
 const Info = ({ selectedSound }) => (
-  <div className='flex flex-row justify-start items-start dn-sh'>
+  <div className='flex flex-row justify-start items-start dn-sh pb2'>
     <img
       className='pt2 w3 dontScale'
       src={selectedSound.artwork_url || selectedSound.userPic}
       alt='artwork'
     />
-    <div className='overflow-auto'>
-      <p className='measure-wide pt2 pl2 infoHeight'>
+    <div className='overflow-auto pt2 pl2'>
+      <p className='measure-wide infoHeight'>
         {takeFirst(
           selectedSound.description.split('\n').filter(s => s.includes('Opis'))
         ).substring(5)}
