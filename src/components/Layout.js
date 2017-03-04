3d23bd067def9f2b0fff8fb1b6d10fe98bbceb91
endziu <koper.andrzej@gmail.com>
2017-03-04 22:41:06 +0100

    handle new route

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,13 +9,14 @@ const sounds = randomizeList(data)
 
 class Layout extends Component {
   render () {
-    const { mapa } = this.props
+    const { mapa, info } = this.props
     return (
       <div className='App flex-auto flex-ns flex-row-ns justify-end f6 black-80 bg-white'>
         { this.props.params.id ? <Menu /> : null }
         <div className='App-main w-100 vh-40 vh-100-ns fadeIn animated ph1 pt1'>
           {this.props.children}
           {mapa ? { mapa } : null}
+          {info ? { info } : null}
         </div>
         <div className='flex flex-column vh-60 vh-100-ns w-100 mw6-ns ph1 bl-m bl-l fadeIn animated'>
           <Sound currentSound={getSoundById(this.props.params.id, sounds)} />
