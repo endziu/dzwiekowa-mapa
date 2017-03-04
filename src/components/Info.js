5675d83e7c3b84285577c56a7c06b71c605210bc
endziu <koper.andrzej@gmail.com>
2017-03-04 04:21:15 +0100

    remove unnecessary padding

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,11 +4,11 @@ import { takeFirst } from '../helpers/'
 const Info = ({ selectedSound }) => (
   <div className='flex flex-row justify-start items-start dn-sh pb2'>
     <img
-      className='pt2 w3 dontScale'
+      className='w3 dontScale'
       src={selectedSound.artwork_url || selectedSound.userPic}
       alt='artwork'
     />
-    <div className='overflow-auto pt2 pl2'>
+    <div className='overflow-auto pl2'>
       <p className='measure-wide infoHeight'>
         {takeFirst(
           selectedSound.description.split('\n').filter(s => s.includes('Opis'))
