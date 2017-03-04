2dd57e1a3a1d975d9216a433a47e4a7156c98504
endziu <koper.andrzej@gmail.com>
2017-03-04 22:38:53 +0100

    rename info.js

---
+++ src/components/Description.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { takeFirst } from '../helpers/'
+
+const Description = ({ selectedSound }) => (
+  <div className='flex flex-row justify-start items-start dn-sh mt1 bg-near-white'>
+    <img
+      className='w3 dontScale'
+      src={selectedSound.artwork_url || selectedSound.userPic}
+      alt='artwork'
+    />
+    <div className='overflow-auto pl2'>
+      <p className='measure-wide infoHeight'>
+        {takeFirst(
+          selectedSound.description.split('\n').filter(s => s.includes('Opis'))
+        ).substring(5)}
+      </p>
+    </div>
+  </div>
+)
+
+export default Description
