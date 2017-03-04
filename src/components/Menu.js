687c92b75e418461433f5e07eb1978779bfa3873
endziu <koper.andrzej@gmail.com>
2017-03-04 02:10:04 +0100

    remove text from buttons

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,54 +3,59 @@ import { Link } from 'react-router'
 
 const Menu = () => (
   <div className='onTop absolute left-1 top-1 fadeIn animated'>
-    <nav className='flex flex-column'>
+    <nav className='flex flex-column-ns flex-row'>
       <Link
         to={'/'}
-        className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1'>
+        title={'Menu'}
+        className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
         <svg className='w1' viewBox='0 0 16 16' fill='none' stroke='currentcolor' strokeWidth='3'>
           <path d='M0 2.5 L16 2.5' />
           <path d='M0 8 L16 8' />
           <path d='M0 13.5 L16 13.5' />
         </svg>
-        <span className='pl1'>Menu</span>
+
       </Link>
 
       <Link
         to={'/'}
-        className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1'>
+        title={'Home'}
+        className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
         <svg className='w1' viewBox='0 0 32 32' fill='currentcolor'>
           <title>Home icon</title>
           <path d='M16 0 L32 16 L28 16 L28 30 L20 30 L20 20 L12 20 L12 30 L4 30 L4 16 L0 16 Z' />
         </svg>
-        <span className='pl1'>Home</span>
+
       </Link>
 
       <Link
         to={'/'}
-        className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1'>
+        title={'Pics'}
+        className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
         <svg className='w1' viewBox='0 0 24 24' fill='currentcolor'>
           <path d='M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z' />
         </svg>
-        <span className='pl1'>Pics</span>
+
       </Link>
 
       <Link
         to={'/'}
-        className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1'>
+        title={'Add Sound'}
+        className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
         <svg className='w1' viewBox='0 0 16 16' stroke='currentcolor' fill='none' strokeWidth='3'>
           <path d='M0 8 L16 8' />
           <path d='M8 0 L8 16' />
         </svg>
-        <span className='pl1'>Add</span>
+
       </Link>
 
       <Link
         to={'/'}
-        className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1'>
+        title={'Info'}
+        className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
         <svg className='w1' viewBox='0 0 24 24' fill='currentcolor'>
           <path d="M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z" />
         </svg>
-        <span className='pl1'>Info</span>
+
       </Link>
 
     </nav>
