4ebdf552953bd5e5cdcb1bb8ec311f2418df085b
endziu <koper.andrzej@gmail.com>
2017-03-04 02:07:56 +0100

    add return keyword

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const SearchField = () => (
-  <form className='pv1'>
-    <input
-      className='input-reset bn pl2 pv2 w5 dn-sh'
-      id='search'
-      placeholder='🔎 Szukaj dźwięków...'
-      type='text' />
-  </form>
-)
+const SearchField = () => {
+  return (
+    <form className='pv1'>
+      <input
+        className='input-reset bn pl2 pv2 dn-sh'
+        id='search'
+        placeholder='🔎 Szukaj dźwięków...'
+        type='text' />
+    </form>
+  )
+}
 
 export default SearchField
