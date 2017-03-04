a0c9f8b040c6798ba8f6b99c8d66409d94871d6c
endziu <koper.andrzej@gmail.com>
2017-03-04 01:27:51 +0100

    make subheading's margins smaller

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = ({ message }) => <header>
   <div className='cover bg-left bg-center-l'>
     <div className='tc-l ph3'>
       <h1 className='baskerville f2 f1-m f-headline-l fw2 black-90 mb0 lh-title'>dźwiękowa mapa Wrocławia</h1>
-      <h2 className='fw1 f2-l f3 black-80 mt3 mb4'>stylish subheadline with extra info</h2>
+      <h2 className='fw1 f2-l f3 black-80 mt2 mb2'>stylish subheadline with extra info</h2>
       <Link
         to={'/rec/235266599'}
         className='f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa2 ba border-box mr1'>
