95f463ed1615d0f4dec2af0b73bd69034d4fdc02
endziu <koper.andrzej@gmail.com>
2017-03-05 04:23:11 +0100

    pass current sound and route to children

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,10 +11,13 @@ class Layout extends Component {
     return (
       <div className='App flex-auto flex-ns flex-row-ns justify-end f6 black-80 bg-white'>
         { this.props.params.id ? <Menu currentId={this.props.params.id} /> : null }
-        <div className='App-main w-100 vh-40 vh-100-ns fadeIn animated ph1 pt1 fadeInDown animated'>
-          {this.props.children}
+        <div className='App-main w-100 vh-40 vh-100-ns fadeIn animated ph1 pt1 fadeIn animated'>
+          {this.props.children && React.cloneElement(this.props.children, {
+            currentSound,
+            currentRoute: this.props.location.pathname
+          })}
         </div>
-        <div className='flex flex-column vh-60 vh-100-ns w-100 mw6-ns ph1 bl-m bl-l fadeInRight animated'>
+        <div className='flex flex-column vh-60 vh-100-ns w-100 mw6-ns ph1 bl-m bl-l fadeIn animated'>
           <Sound currentSound={currentSound} />
           <List currentId={this.props.params.id} sounds={sounds} />
         </div>
