528cd873ba172d01aec7f7ae0ec221ab4112423e
endziu <koper.andrzej@gmail.com>
2017-03-05 04:22:38 +0100

    show some data

--- src/components/Imgs.js
+++ src/components/Imgs.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Imgs = (props) => <div className='pa5'>
-  <h1>imgs</h1>
-  <p>{props.params.id}</p>
+const Imgs = (props) => <div className='pl2 pl5-ns pt5 pt2-ns'>
+  <p>{`id: ${props.params.id}`}</p>
+  <p>{`route: ${props.currentRoute}`}</p>
+  <p>
+    <img title={'avatar'} src={props.currentSound.userPic} alt={'userPic'} />
+    <img title={'artwork'} src={props.currentSound.artwork_url} alt={'artwork'} />
+  </p>
 </div>
 
 export default Imgs
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Info = (props) => <div className='pa5'>
-  <h1>info</h1>
-  <p>{props.params.id}</p>
+const Info = (props) => <div className='pl2 pl5-ns pt5 pt2-ns'>
+  <p>{`id: ${props.params.id}`}</p>
+  <p>{`route: ${props.currentRoute}`}</p>
+  <p>{`${props.currentSound.userName} - ${props.currentSound.title}`}</p>
+  <p>{`duration: ${props.currentSound.duration} ms.`}</p>
 </div>
 
 export default Info
