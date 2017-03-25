18fe2ea788425c82c804a7373c00b80845991ad8
endziu <koper.andrzej@gmail.com>
2017-03-25 22:37:56 +0100

    minor style changes

--- src/shared/comps/Menu.js
+++ src/shared/comps/Menu.js
@@ -6,6 +6,15 @@ const Menu = ({id}) => {
     <div className='onTop menuPosition'>
       <div className='flex flex-row flex-column-ns content-end animated fadeIn'>
         <Link
+          to={`/info/${id}`}
+          title={'Wiecej Informacji'}
+          className='f5 w1-8 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
+          <svg className='w1-8' viewBox='0 0 24 24' fill='currentcolor'>
+            <path d='M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z' />
+          </svg>
+        </Link>
+
+        <Link
           to={'/'}
           title={'Home'}
           className='f5 w1-8 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
@@ -15,15 +24,6 @@ const Menu = ({id}) => {
           </svg>
         </Link>
 
-        <Link
-          to={`/info/${id}`}
-          title={'Wiecej Informacji'}
-          className='f5 w1-8 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
-          <svg className='w1-8' viewBox='0 0 24 24' fill='currentcolor'>
-            <path d='M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z' />
-          </svg>
-        </Link>
-        
       </div>
     </div>
   )
--- src/shared/comps/SearchField.js
+++ src/shared/comps/SearchField.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchField = () => {
   return (
-    <form className='absolute onTop'>
+    <form className='ma1 absolute onTop'>
       <input
         className='input-reset pl2 pv2 bn'
         id='search'
