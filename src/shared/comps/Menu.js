0b3b07563d7834d09a664da9d5da1ea16ceaed86
endziu <koper.andrzej@gmail.com>
2017-03-25 22:30:53 +0100

    remove search button

--- src/shared/comps/Menu.js
+++ src/shared/comps/Menu.js
@@ -23,15 +23,7 @@ const Menu = ({id}) => {
             <path d='M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z' />
           </svg>
         </Link>
-
-        <Link
-          to={'/search'}
-          title={'szukaj'}
-          className='f5 w1-8 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
-          <svg className='w1-8' fill='currentColor' viewBox='0 0 32 32'>
-            <path d='M12 0 A12 12 0 0 0 0 12 A12 12 0 0 0 12 24 A12 12 0 0 0 18.5 22.25 L28 32 L32 28 L22.25 18.5 A12 12 0 0 0 24 12 A12 12 0 0 0 12 0 M12 4 A8 8 0 0 1 12 20 A8 8 0 0 1 12 4' />
-          </svg>
-        </Link>
+        
       </div>
     </div>
   )
