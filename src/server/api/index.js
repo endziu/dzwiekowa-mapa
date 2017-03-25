4d42ff32547e3b2ddaa2e69fe5e3587166dfadd7
endziu <koper.andrzej@gmail.com>
2017-03-25 20:52:52 +0100

    format

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,2 +1,1 @@
-
 export default (req, res) => res.status(200).send({'bibi': 'boo'})
--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,15 @@
 import express from 'express'
 import favicon from 'express-favicon'
 import compression from 'compression'
-import sourceMapSupport from 'source-map-support'
-import renderApp from './render'
 import api from './api'
-
-const app = express()
+import renderApp from './render'
+import sourceMapSupport from 'source-map-support'
 sourceMapSupport.install()
 
+const app = express()
 app.use(compression({filter: (req) => req.headers['x-no-compression'] ? true : false}))
 app.use(favicon(__dirname + '/src/shared/assets/favicon.png'))
 app.use('/static', express.static('./dist'))
 app.use('/api', api)
 app.use(['/', '/rec/:id'], renderApp)
-
 app.listen(3000, () => console.log('Demo app listening on port 3000'))
