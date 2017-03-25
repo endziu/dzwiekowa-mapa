6a515e7ba9ab8d9aa16be562bb5a9183ede69663
endziu <koper.andrzej@gmail.com>
2017-03-25 15:12:16 +0100

    standard js

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -1,42 +1,42 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const srcPath = path.resolve(__dirname, 'src');
-const distPath = path.resolve(__dirname, 'dist');
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const srcPath = path.resolve(__dirname, 'src')
+const distPath = path.resolve(__dirname, 'dist')
 
 module.exports = {
-    context: srcPath,
-    target: 'web',
+  context: srcPath,
+  target: 'web',
 
-    entry: {
-        client: './client/index.js'
-    },
-    output: {
-        path: distPath,
-        filename: '[name].js',
-        publicPath: '/'
-    },
-    resolve: {
-        modules: ['node_modules', 'src'],
-        extensions: ['*', '.js', '.json']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: 'css-loader'
-                })
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('style.css')
-    ],    
-    devtool: 'source-map'
-};
+  entry: {
+    client: './client/index.js'
+  },
+  output: {
+    path: distPath,
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  resolve: {
+    modules: ['node_modules', 'src'],
+    extensions: ['*', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ],
+  devtool: 'source-map'
+}
 
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,46 +1,46 @@
-const nodeExternals = require('webpack-node-externals');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const path = require('path');
-const srcPath = path.resolve(__dirname, 'src');
-const distPath = path.resolve(__dirname, 'dist');
+const nodeExternals = require('webpack-node-externals')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path')
+const srcPath = path.resolve(__dirname, 'src')
+const distPath = path.resolve(__dirname, 'dist')
 
 module.exports = {
-    context: srcPath,
-    entry: {
-        server: './server/index.js'
-    },
-    output: {
-        path: distPath,
-        filename: '[name].js'
-    },
-    target: 'node',
-    node: {
-        __dirname: false,
-        __filename: false
-    },
-    resolve: {
-        modules: ['node_modules', 'src'],
-        extensions: ['*', '.js', '.json']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: 'css-loader'
-                })
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('style.css')
-    ],  
-    externals: nodeExternals(),
-    devtool: 'source-map'
-};
+  context: srcPath,
+  entry: {
+    server: './server/index.js'
+  },
+  output: {
+    path: distPath,
+    filename: '[name].js'
+  },
+  target: 'node',
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  resolve: {
+    modules: ['node_modules', 'src'],
+    extensions: ['*', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ],
+  externals: nodeExternals(),
+  devtool: 'source-map'
+}
