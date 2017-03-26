3b4d1937c54719a41ea2a42c6189d642888f0b74
endziu <koper.andrzej@gmail.com>
2017-03-26 19:45:04 +0200

    add helmet

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import favicon from 'express-favicon'
+import helmet from 'helmet'
 import compression from 'compression'
 import api from './api'
 import renderApp from './render'
@@ -7,6 +8,7 @@ import sourceMapSupport from 'source-map-support'
 sourceMapSupport.install()
 
 const app = express()
+app.use(helmet())
 app.use(compression({filter: (req) => req.headers['x-no-compression'] ? true : false}))
 app.use(favicon(__dirname + '/src/shared/assets/favicon.png'))
 app.use('/static', express.static('./dist'))
