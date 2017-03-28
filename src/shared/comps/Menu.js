917f12951ae2af7df23af354f8341a00471d1815
endziu <koper.andrzej@gmail.com>
2017-03-28 19:09:38 +0200

    fix menu

--- src/shared/comps/Menu.js
+++ src/shared/comps/Menu.js
@@ -4,16 +4,7 @@ import { Link } from 'react-router-dom'
 const Menu = ({id}) => {
   return (
     <div className='onTop menuPosition'>
-      <div className='flex flex-row flex-column-ns content-end animated fadeIn'>
-        <Link
-          to={`/info/${id}`}
-          title={'Wiecej Informacji'}
-          className='f5 w1-8 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
-          <svg className='w1-8' viewBox='0 0 24 24' fill='currentcolor'>
-            <path d='M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z' />
-          </svg>
-        </Link>
-
+      <div className='flex flex-column content-end animated fadeIn'>
         <Link
           to={'/'}
           title={'Home'}
@@ -24,6 +15,14 @@ const Menu = ({id}) => {
           </svg>
         </Link>
 
+        <Link
+          to={`/info/${id}`}
+          title={'Wiecej Informacji'}
+          className='f5 w1-8 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
+          <svg className='w1-8' viewBox='0 0 24 24' fill='currentcolor'>
+            <path d='M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z' />
+          </svg>
+        </Link>
       </div>
     </div>
   )
