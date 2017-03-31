44ded426b31b306025d5757462e3034419a9084a
endziu <koper.andrzej@gmail.com>
2017-03-31 16:31:21 +0200

    render from file

--- src/server/render/template.js
+++ src/server/render/template.js
@@ -1,9 +1,16 @@
 import { renderToString } from 'react-dom/server'
+import readFile from 'fs-readfile-promise'
 
 export default (app, data) => {
   const appString = renderToString(app(data))
   const dataString = `window.__sounds__ = ${JSON.stringify(data)}`
-  
+
+const prepareHtml = () => {
+  readFile('./src/index.html', 'utf-8')
+    .then((s) => s.replace('{{DATA}}', dataString))
+    .then((s) => s.replace('{{APP}}', appString))
+}
+
   return `
     <!DOCTYPE html>
     <html lang="pl">
