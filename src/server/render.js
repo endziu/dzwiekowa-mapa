8e86464ec9ddb7abd1b327558a761eb44531fee0
endziu <koper.andrzej@gmail.com>
2017-03-31 21:46:17 +0200

    rename

--- src/server/render.js
+++ src/server/render.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import readFile from 'fs-readfile-promise'
-import { renderToString as reactToHtml } from 'react-dom/server'
+import { renderToString } from 'react-dom/server'
 import { StaticRouter as Router, matchPath } from 'react-router'
 
 import App from '../shared/App'
@@ -17,13 +17,13 @@ export default (req, res) => {
       .then(file => JSON.parse(file))
       .then(data => {
         return readFile('./src/index.html', 'utf-8')
-          .then((s) => s.replace('{{APP}}', reactToHtml(routerApp(data))))
+          .then((s) => s.replace('{{APP}}', renderToString(routerApp(data))))
           .then((s) => s.replace('{{DATA}}', `window.__sounds__ = ${JSON.stringify(data)}`))
       })
       .then((html) => res.status(200).send(html))
-      .catch(err => res.status(500).send(reactToHtml(<Error message={err} />)))
+      .catch((err) => res.status(500).send(renderToString(<Error message={err} />)))
   } else {
-    res.status(404).send(reactToHtml(<NoMatch />))
+    res.status(404).send(renderToString(<NoMatch />))
     return
   }
 }
