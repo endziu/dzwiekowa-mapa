9637ad70128b2bebed33bece9161a9f32b78dfb2
endziu <koper.andrzej@gmail.com>
2017-03-31 18:12:24 +0200

    read json with utf-8

--- src/server/render.js
+++ src/server/render.js
@@ -1,7 +1,7 @@
 import React from 'react'
+import readFile from 'fs-readfile-promise'
 import { renderToString as reactToHtml } from 'react-dom/server'
 import { StaticRouter as Router, matchPath } from 'react-router'
-import readFile from 'fs-readfile-promise'
 
 import App from '../shared/App'
 import Error from '../shared/comps/Error'
@@ -10,14 +10,14 @@ import NoMatch from '../shared/comps/NoMatch'
 export default (req, res) => {
   const matchHome = matchPath(req.url, { path: '/', exact: true })
   const matchSound = matchPath(req.url, { path: '/rec/:id' })
-  const reactApp = (data) => (<Router context={{}} location={req.url}><App sounds={data} /></Router>)
+  const routerApp = (data) => <Router context={{}} location={req.url}><App sounds={data} /></Router>
 
   if (matchHome || matchSound) {
-    readFile('./src/shared/assets/tracks.json')
-      .then(file => JSON.parse(file.toString()))
+    readFile('./src/shared/assets/tracks.json', 'utf-8')
+      .then(file => JSON.parse(file))
       .then(data => {
         return readFile('./src/index.html', 'utf-8')
-          .then((s) => s.replace('{{APP}}', reactToHtml(reactApp(data))))
+          .then((s) => s.replace('{{APP}}', reactToHtml(routerApp(data))))
           .then((s) => s.replace('{{DATA}}', `window.__sounds__ = ${JSON.stringify(data)}`))
       })
       .then((html) => res.status(200).send(html))
@@ -26,5 +26,4 @@ export default (req, res) => {
     res.status(404).send(reactToHtml(<NoMatch />))
     return
   }
-  
 }
