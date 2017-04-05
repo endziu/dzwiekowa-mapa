75d253b600877f443fc71dea9a626ad9097cd32e
endziu <koper.andrzej@gmail.com>
2017-04-05 20:10:55 +0200

    add map menu-button

--- src/shared/comps/Info.js
+++ src/shared/comps/Info.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default () => <div className='center infoSize'>
-  <div className='w-25 h-25'>something</div>
-  <div className='w-25 h-25'>that</div>
-  <div className='w-25 h-25'>streches across</div>
+  <div className='w-25 h-25'>something centered</div>
+  <div className='w-25 h-25'>that streches</div>
+  <div className='w-25 h-25'>across</div>
   <div className='w-25 h-25'>the page</div>
 </div>
--- src/shared/comps/Menu.js
+++ src/shared/comps/Menu.js
@@ -23,6 +23,16 @@ const Menu = ({id}) => {
             <path d='M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z' />
           </svg>
         </Link>
+        
+        <Link
+          to={`/rec/${id}`}
+          title={'Mapa'}
+          className='f5 w1-8 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
+          <svg className='w1-8' viewBox='0 0 24 24' fill='currentcolor'>
+            <path d='M20.5 3l-.16.03L15 5.1 9 3 3.36 4.9c-.21.07-.36.25-.36.48V20.5c0 .28.22.5.5.5l.16-.03L9 18.9l6 2.1 5.64-1.9c.21-.07.36-.25.36-.48V3.5c0-.28-.22-.5-.5-.5zM15 19l-6-2.11V5l6 2.11V19z' />
+          </svg>
+        </Link>
+
       </div>
     </div>
   )
