1d5b1d8f925c9536fbd7ed8abc31f4bbe879dc24
endziu <koper.andrzej@gmail.com>
2017-04-05 19:55:12 +0200

    change txt

--- src/shared/comps/Info.js
+++ src/shared/comps/Info.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default () => <div className='center infoSize'>
-  <div className='w-25 h-25'>paap</div>
-  <div className='w-25 h-25'>poop</div>
-  <div className='w-25 h-25'>peep</div>
-  <div className='w-25 h-25'>piip</div>
+  <div className='w-25 h-25'>something</div>
+  <div className='w-25 h-25'>that</div>
+  <div className='w-25 h-25'>streches across</div>
+  <div className='w-25 h-25'>the page</div>
 </div>
