c063b654172503d0593a7043261485cf6a3018f6
endziu <koper.andrzej@gmail.com>
2017-04-05 22:42:54 +0200

    adjust font blackness

--- src/shared/comps/Welcome.js
+++ src/shared/comps/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = () => <header className='vh-100 w-100'>
   <div className='bg-near-white h-100'>
     <div className='tc-ns pa3'>
       <h1 className='f2 f1-m f-headline-l fw2 black mt2 mb3 lh-title'><a className='link'>dźwiękowa mapa Wrocławia</a></h1>
-      <h2 className='fw1 f2-l f3 black-60 mv3'>kolekcja nagrań audiosfery miasta</h2>
+      <h2 className='fw1 f2-l f3 black-80 mv3'>kolekcja nagrań audiosfery miasta</h2>
       <Link
         to={'/rec/313303431'}
         className='f5 no-underline white bg-black-80 bg-animate hover-bg-white hover-black inline-flex items-center pa2 ba border-box mv3'>
