dce924ef9250763514cb48738c7901bc6d104581
endziu <koper.andrzej@gmail.com>
2017-04-07 19:59:22 +0200

    add info style

--- src/shared/comps/Info.js
+++ src/shared/comps/Info.js
@@ -5,56 +5,47 @@ export default ({ currentSound }) => {
   console.log(desc)
   return (
     <div className="center pa3 pa4-ns w-100 vh-40 vh-100-ns overflow-auto">
-      <div className="tc">
+      <div className="flex mb3">
         <img
           title="user avatar"
           className="br-100 h3 w3 dib ba b--black-05 pa1"
           src={currentSound.userPic}
         />
-        <h2 className="f4 mb2">{currentSound.userName}</h2>
-        <h3 className="f6 fw4 gray mt0">{currentSound.title}</h3>     
+        <div className='flex flex-column pt2 pl2'>
+          <h2 className="f5 mb2">{currentSound.userName}</h2>
+          <h3 className="f6 fw4 gray mt0">{currentSound.title}</h3>     
+        </div>
       </div>
-      <div className='tc pa3'>
+      <div className='flex flex-column'>
         <p className='pb1'>
           {desc
             .map(s => s.toLowerCase())
-            .filter(s => s.search('autor nagrania:') !== -1)
-            .map(s => s.substring(15))[0]
-          }
+            .filter(s => s.search('autor nagrania:') !== -1)[0]}
         </p>        
         <p className='pb1'>
           {desc
             .map(s => s.toLowerCase())
-            .filter(s => s.search('lokalizacja:') !== -1)
-            .map(s => s.substring(12))[0]
-          }
+            .filter(s => s.search('lokalizacja:') !== -1)[0]}
         </p>
         <p className='pb1'>
           {desc
             .map(s => s.toLowerCase())
-            .filter(s => s.search('gps:') !== -1)
-            .map(s => s.substring(5))[0]
-          }
+            .filter(s => s.search('gps:') !== -1)[0]}
         </p>
         <p className='pb1'>
           {desc
             .map(s => s.toLowerCase())
-            .filter(s => s.search('rekorder, mikrofon:') !== -1)
-            .map(s => s.substring(19))[0]
-          }
+            .filter(s => s.search('rekorder, mikrofon:') !== -1)[0]}
         </p>
         <p className='pb1'>
           {desc
             .map(s => s.toLowerCase())
-            .filter(s => s.search('czas:') !== -1)[0]
-          }
+            .filter(s => s.search('czas:') !== -1)[0]}
         </p>
         <p className='pb1'>
           {desc
             .map(s => s.toLowerCase())
-            .filter(s => s.search('data:') !== -1)
-            .map(s => s.substring(5))[0]
-          }
+            .filter(s => s.search('data:') !== -1)[0]}
         </p>
       </div>
     </div>
