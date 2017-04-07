b305e18ec741edcc8af762576dafd66e689f68f5
endziu <koper.andrzej@gmail.com>
2017-04-07 18:23:37 +0200

    add user and title info

--- src/shared/comps/Info.js
+++ src/shared/comps/Info.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-export default () => <div className='center infoSize'>
-  <div className='w-25 h-25'>something centered</div>
-  <div className='w-25 h-25'>that streches</div>
-  <div className='w-25 h-25'>across</div>
-  <div className='w-25 h-25'>the page</div>
-</div>
+export default ({ currentSound }) => {
+  const desc = currentSound.description.split('\n')
+  desc.map((item,index) => console.log(index, item))
+  return (
+    <article className="center pa3 pa4-ns w-100 vh-40 overflow-auto">
+      <div className="tc">
+        <img
+          title="user avatar"
+          className="br-100 h3 w3 dib ba b--black-05 pa1"
+          src={currentSound.userPic}
+        />
+        <h2 className="f4 mb2">{currentSound.userName}</h2>
+        <h3 className="f6 fw4 gray mt0">{currentSound.title}</h3>     
+      </div>
+    </article>
+  )
+}
