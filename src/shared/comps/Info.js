6b8096cba1b98b98db85b0c47979b63ab8d4363d
endziu <koper.andrzej@gmail.com>
2017-04-07 19:01:17 +0200

    add some info

--- src/shared/comps/Info.js
+++ src/shared/comps/Info.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default ({ currentSound }) => {
   const desc = currentSound.description.split('\n')
-  desc.map((item,index) => console.log(index, item))
+  console.log(desc)
   return (
-    <article className="center pa3 pa4-ns w-100 vh-40 overflow-auto">
+    <div className="center pa3 pa4-ns w-100 vh-40 vh-100-ns overflow-auto">
       <div className="tc">
         <img
           title="user avatar"
@@ -14,6 +14,50 @@ export default ({ currentSound }) => {
         <h2 className="f4 mb2">{currentSound.userName}</h2>
         <h3 className="f6 fw4 gray mt0">{currentSound.title}</h3>     
       </div>
-    </article>
+      <div className='pa3'>
+        <p className='pb1'>
+          {desc
+            .map(s => s.toLowerCase())
+            .filter(s => s.search('gps:') !== -1)
+            .map(s => s.substring(5))[0]
+          }
+        </p>
+        <p className='pb1'>
+          {desc
+            .map(s => s.toLowerCase())
+            .filter(s => s.search('lokalizacja:') !== -1)
+            .map(s => s.substring(12))[0]
+          }
+        </p>
+        <p className='pb1'>
+          {desc
+            .map(s => s.toLowerCase())
+            .filter(s => s.search('rekorder, mikrofon:') !== -1)
+            .map(s => s.substring(19))[0]
+          }
+        </p>
+        <p className='pb1'>
+          {desc
+            .map(s => s.toLowerCase())
+            .filter(s => s.search('czas:') !== -1)
+            .map(s => s.substring(5))[0]
+          }
+        </p>
+        <p className='pb1'>
+          {desc
+            .map(s => s.toLowerCase())
+            .filter(s => s.search('data:') !== -1)
+            .map(s => s.substring(5))[0]
+          }
+        </p>
+        <p className='pb1'>
+          {desc
+            .map(s => s.toLowerCase())
+            .filter(s => s.search('autor nagrania:') !== -1)
+            .map(s => s.substring(15))[0]
+          }
+        </p>        
+      </div>
+    </div>
   )
 }
