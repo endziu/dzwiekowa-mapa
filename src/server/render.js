beaa5c03e9b2e839250050132a1665f303f9ddb1
endziu <koper.andrzej@gmail.com>
2017-04-12 14:48:05 +0200

    handle invalid requests

--- src/server/render.js
+++ src/server/render.js
@@ -1,24 +1,27 @@
 import React from 'react'
+import path from 'path'
 import readFile from 'fs-readfile-promise'
 import { renderToString } from 'react-dom/server'
 import { StaticRouter, matchPath } from 'react-router'
 
 import App from '../shared/App'
 import Error from '../shared/comps/Error'
+import { asNumber, isNum } from '../shared/helpers'
 import NoMatch from '../shared/comps/NoMatch'
 
 export default (req, res, next) => {
   const matchHome = matchPath(req.url, { path: '/', exact: true })
-  const matchMap = matchPath(req.url, { path: '/:id/map', exact: true })
   const matchInfo = matchPath(req.url, { path: '/:id/info', exact: true })
+  const matchMap = matchPath(req.url, { path: '/:id/map', exact: true })
+  const ID = path.parse(req.url).dir.substring(1)
 
-  const routerApp =
-    (data) =>
-      <StaticRouter context={{}} location={req.url}>
-        <App sounds={data} />
-      </StaticRouter>
+  if (matchHome || (matchInfo && isNum(ID)) || (matchMap && isNum(ID))) {
+    const routerApp =
+      (data) =>
+        <StaticRouter context={{}} location={req.url}>
+          <App sounds={data} />
+        </StaticRouter>
 
-  if (matchHome || matchMap || matchInfo) {
     return readFile('./src/shared/assets/tracks.json', 'utf-8')
       .then(file => JSON.parse(file))
       .then(jsonData => {
