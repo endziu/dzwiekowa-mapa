c3d0358a59f80fa00d85a4c346eaecee99ddafbd
endziu <koper.andrzej@gmail.com>
2017-04-19 19:00:39 +0200

    switch order

--- src/shared/comps/Menu.js
+++ src/shared/comps/Menu.js
@@ -16,6 +16,15 @@ const Menu = ({id}) => {
         </Link>
 
         <Link
+          to={`/${id}/map`}
+          title={'Mapa'}
+          className='f5 w1-8 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
+          <svg className='w1-8' viewBox='0 0 24 24' fill='currentcolor'>
+            <path d='M20.5 3l-.16.03L15 5.1 9 3 3.36 4.9c-.21.07-.36.25-.36.48V20.5c0 .28.22.5.5.5l.16-.03L9 18.9l6 2.1 5.64-1.9c.21-.07.36-.25.36-.48V3.5c0-.28-.22-.5-.5-.5zM15 19l-6-2.11V5l6 2.11V19z' />
+          </svg>
+        </Link>
+
+        <Link
           to={`/${id}/info`}
           title={'Wiecej Informacji'}
           className='f5 w1-8 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
@@ -30,15 +39,6 @@ const Menu = ({id}) => {
           className='f5 w1-8 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
           <svg className='w1-8' viewBox='0 0 24 24' fill='currentcolor'>
             <path d='M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z' />
-          </svg>
-        </Link>
-
-        <Link
-          to={`/${id}/map`}
-          title={'Mapa'}
-          className='f5 w1-8 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
-          <svg className='w1-8' viewBox='0 0 24 24' fill='currentcolor'>
-            <path d='M20.5 3l-.16.03L15 5.1 9 3 3.36 4.9c-.21.07-.36.25-.36.48V20.5c0 .28.22.5.5.5l.16-.03L9 18.9l6 2.1 5.64-1.9c.21-.07.36-.25.36-.48V3.5c0-.28-.22-.5-.5-.5zM15 19l-6-2.11V5l6 2.11V19z' />
           </svg>
         </Link>
 
