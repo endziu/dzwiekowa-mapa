d26f49636de35f3977ad1fbbd263d4fb7e5d35a7
endziu <koper.andrzej@gmail.com>
2017-04-24 00:35:49 +0200

    add check back

--- src/server/render.js
+++ src/server/render.js
@@ -2,19 +2,28 @@ import React from 'react'
 import path from 'path'
 import readFile from 'fs-readfile-promise'
 import { renderToString } from 'react-dom/server'
-import { StaticRouter } from 'react-router'
+import { StaticRouter, matchPath } from 'react-router'
 
 import App from '../shared/App'
 import Error from '../shared/comps/Error'
 import NoMatch from '../shared/comps/NoMatch'
 import { asNumber, isDigitOnly } from '../shared/helpers'
 
-export default (req, res, next) => {  
+export default (req, res, next) => {
+  //['/', '/:id', '/:id/info', '/:id/map', '/:id/photos']
+  const matchHome = matchPath(req.url, { path: '/', exact: true })
+  const matchSound = matchPath(req.url, { path: '/:id', exact: true })
+  const matchInfo = matchPath(req.url, { path: '/:id/info', exact: true })
+  const matchMap = matchPath(req.url, { path: '/:id/map', exact: true })
+  const matchPhotos = matchPath(req.url, { path: '/:id/photos', exact: true })
+
   const routerApp =
     (data) =>
       <StaticRouter context={{}} location={req.url}>
         <App sounds={data} />
       </StaticRouter>
+
+  if (matchHome || matchSound || matchInfo || matchMap || matchPhotos) {
 
   readFile('./src/shared/assets/tracks.json', 'utf-8')
     .then(file => JSON.parse(file))
@@ -37,4 +46,8 @@ export default (req, res, next) => {
       res.status(500).send(renderToString(<Error />))
       next()
     })
+  } else {
+    res.status(404).send(renderToString(<NoMatch />))
+    next()
+  }
 }
