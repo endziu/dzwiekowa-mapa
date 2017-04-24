8aef1e7f137d4396a85a3c380e43c21d42b16596
endziu <koper.andrzej@gmail.com>
2017-04-24 17:06:37 +0200

    change link title

--- src/shared/comps/Menu.js
+++ src/shared/comps/Menu.js
@@ -26,7 +26,7 @@ const Menu = ({id}) => {
 
         <Link
           to={`/${id}/info`}
-          title={'Wiecej Informacji'}
+          title={'Info'}
           className='f5 w1-8 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa1 ba border-box mb1 mr1'>
           <svg className='w1-8' viewBox='0 0 24 24' fill='currentcolor'>
             <path d='M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z' />
