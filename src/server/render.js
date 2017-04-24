6b1d46b863fdf67c55d0462f4f16da77cf8ffa44
endziu <koper.andrzej@gmail.com>
2017-04-24 12:59:25 +0200

    refactor

--- src/server/render.js
+++ src/server/render.js
@@ -10,32 +10,25 @@ import NoMatch from '../shared/comps/NoMatch'
 import { asNumber, isDigitOnly } from '../shared/helpers'
 
 export default (req, res, next) => {
-  //['/', '/:id', '/:id/info', '/:id/map', '/:id/photos']
-  const matchHome = matchPath(req.url, { path: '/', exact: true })
-  const matchSound = matchPath(req.url, { path: '/:id', exact: true })
-  const matchInfo = matchPath(req.url, { path: '/:id/info', exact: true })
-  const matchMap = matchPath(req.url, { path: '/:id/map', exact: true })
-  const matchPhotos = matchPath(req.url, { path: '/:id/photos', exact: true })
+  const routes = ['/', '/:id', '/:id/info', '/:id/map', '/:id/photos']
+  const checkRoutes = (paths) => paths.filter((i) => matchPath(req.url, { path: i, exact: true }))
+  const routeMatches = checkRoutes(routes).length !== 0
 
-  const routerApp =
-    (data) =>
-      <StaticRouter context={{}} location={req.url}>
-        <App sounds={data} />
-      </StaticRouter>
-
-  if (matchHome || matchSound || matchInfo || matchMap || matchPhotos) {
+  if (routeMatches) {
 
   readFile('./src/shared/assets/tracks.json', 'utf-8')
     .then(file => JSON.parse(file))
     .then(data => {
-      const reactApp = routerApp(data)
+      const reactApp =
+        <StaticRouter context={{}} location={req.url}>
+          <App sounds={data} />
+        </StaticRouter>
       const appString = renderToString(reactApp)
       const dataString = JSON.stringify(data)
       const html =
         readFile('./dist/index.html', 'utf-8')
           .then((s) => s.replace('{{APP}}', appString))
           .then((s) => s.replace('{{DATA}}', `window.__sounds__ = ${dataString}`))
-
       return html
     })
     .then((html) => {
