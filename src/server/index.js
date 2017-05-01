329fc8acd682a6b1b68d6cc2d49ee7097122652c
endziu <koper.andrzej@gmail.com>
2017-05-01 12:44:29 +0200

    ser port

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import helmet from 'helmet'
 import compression from 'compression'
 import favicon from 'express-favicon'
@@ -10,12 +11,14 @@ import log from './log'
 import routes from './routes'
 import renderApp from './render'
 
+const PORT = process.env.PORT || 3000
+
 const server = express()
 server.use(helmet())
 server.use(compression())
-server.use(favicon(__dirname + './src/shared/assets/favicon.ico'))
+server.use(favicon(path.join(__dirname, 'public/favicon.png')))
 server.use(log)
 server.use('/static', express.static('./dist'))
 server.use('/api', api)
 server.use(routes, renderApp)
-server.listen(process.env.PORT, () => console.log('dzwiekowa-mapa is listening on port 3000'))
+server.listen(PORT, () => console.log(`dzwiekowa-mapa is listening on port ${PORT}`))
