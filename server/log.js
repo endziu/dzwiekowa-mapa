2bb110ed9a18a21fce93cda399d9c8122c13943b
endziu <koper.andrzej@gmail.com>
2017-05-02 22:08:20 +0200

    rm log file

--- server/log.js
+++
@@ -1,9 +0,0 @@
-import morgan from 'morgan'
-
-const log = morgan((tokens, req, res) =>
-  [ tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res)
-  ].join(' '))
-
-export default log
