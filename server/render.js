399d9e98fe55d997dcd7da3a5d86ebbaca9c974d
endziu <koper.andrzej@gmail.com>
2017-10-23 19:48:31 +0200

    refactor

--- server/render.js
+++ server/render.js
@@ -8,30 +8,28 @@ import Error from '../shared/comps/Error'
 import NoMatch from '../shared/comps/NoMatch'
 import Routes from './routes'
 
-const notEmpty = xs => xs.length > 0
-const matchUrl = url => route => matchPath(url, { path: route, exact: true })
-const routeMatches = (url, routes) => notEmpty(routes.filter(matchUrl(url)))
+const generateHtml = url => data => {
+  const appString = renderToString(
+    <StaticRouter context={{}} location={url}>
+      <App sounds={data} />
+    </StaticRouter>
+  )
+  const dataString = JSON.stringify(data)
+  const html = readFile('./dist/index.html', 'utf-8')
+    .then(s => s.replace('{{APP}}', appString))
+    .then(s => s.replace('{{DATA}}', `window.__sounds__ = ${dataString}`))
+  return html
+}
 
 export default (req, res, next) => {
-  const generateHtml = data => {
-    const appString = renderToString(
-      <StaticRouter context={{}} location={req.url}>
-        <App sounds={data} />
-      </StaticRouter>
-    )
-    const dataString = JSON.stringify(data)
-    const html = readFile('./dist/index.html', 'utf-8')
-      .then(s => s.replace('{{APP}}', appString))
-      .then(s => s.replace('{{DATA}}', `window.__sounds__ = ${dataString}`))
-    return html
-  }
-
-  const validUrl = routeMatches(req.url, Routes)
+  const notEmpty = xs => xs.length > 0
+  const matchUrl = url => route => matchPath(url, { path: route, exact: true })
+  const validUrl = notEmpty(Routes.filter(matchUrl(req.url)))
 
   if (validUrl) {
     readFile('./db/tracks.json', 'utf-8')
       .then(JSON.parse)
-      .then(generateHtml)
+      .then(generateHtml(req.url))
       .then(html => res.status(200).send(html))
       .catch(() => res.status(500).send(renderToString(<Error />)))
   } else {
