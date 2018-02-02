8f1dd813be5e11b3c52f2fba6757c9669eb2d4ca
endziu <koper.andrzej@gmail.com>
2018-02-03 00:35:24 +0100

    style

--- shared/comps/Welcome.js
+++ shared/comps/Welcome.js
@@ -2,41 +2,63 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const urlBase = 'https://images.unsplash.com/photo-1470326032834-47d8a3054bb4'
-const urlParams = '?dpr=1&auto=compress,format&fit=crop&w=1920&h=1080&q=60&cs=tinysrgb&crop=&bg='
+const urlParams =
+  '?dpr=1&auto=compress,format&fit=crop&w=1920&h=1080&q=60&cs=tinysrgb&crop=&bg='
 const unsplashUrl = `${urlBase}${urlParams}`
 
-const Welcome = () => <header className='vh-100 w-100'>
-  <div
-    className='bg-near-white h-100'
-    style={{
-      backgroundImage: `url(${unsplashUrl})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'left',
-      backgroundRepeat: 'no-repeat'
-    }}
-  >
+const Welcome = () => (
+  <header className="vh-100 w-100">
+    <div
+      className="bg-near-white h-100"
+      style={{
+        backgroundImage: `url(${unsplashUrl})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'left',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
 
-    <div className='pa3'>
-      <h1 className='f2 f1-m f-headline-l fw2 black-80 mt2 mb3 lh-title'>
-        <a className='link'>dźwiękowa mapa Wrocławia</a>
-      </h1>
-      <h2 className='fw1 f2-l f3 black-60 mv3'>kolekcja nagrań audiosfery miasta</h2>
-      <Link
-        to={'/fontanna_rynek/map'}
-        className='v-mid f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa2 ba border-box mv3'>
-        <svg className='w1' viewBox='0 0 24 24' style={{fill: 'currentcolor'}}>
-          <title>Map icon</title>
-          <path d='M20.5 3l-.16.03L15 5.1 9 3 3.36 4.9c-.21.07-.36.25-.36.48V20.5c0 .28.22.5.5.5l.16-.03L9 18.9l6 2.1 5.64-1.9c.21-.07.36-.25.36-.48V3.5c0-.28-.22-.5-.5-.5zM15 19l-6-2.11V5l6 2.11V19z' />
-        </svg>
-        <span className='pl1'>Posłuchaj</span>
-      </Link>
-      <footer className='absolute bottom-0 right-0 left-0 f6 pa2 white' style={{background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8) )'}}>
-        <span className='fr'>
-          built with ❤️️ by: <a href='https://endziu.xyz' className='link white hover-bg-white hover-black'>endziu.xyz</a>
-        </span>
-      </footer>
+      <div className="pa3">
+        <h1 className="f2 f1-m f-headline-l fw2 black-80 mt2 mb3 lh-title">
+          <a className="link">dźwiękowa mapa Wrocławia</a>
+        </h1>
+        <h2 className="fw1 f2-l f3 black-60 mv3">
+          kolekcja nagrań audiosfery miasta
+        </h2>
+        <Link
+          to={'/fontanna_rynek/map'}
+          className="v-mid f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa2 ba border-box mv3"
+        >
+          <svg
+            className="w1"
+            viewBox="0 0 24 24"
+            style={{ fill: 'currentcolor' }}
+          >
+            <title>Map icon</title>
+            <path d="M20.5 3l-.16.03L15 5.1 9 3 3.36 4.9c-.21.07-.36.25-.36.48V20.5c0 .28.22.5.5.5l.16-.03L9 18.9l6 2.1 5.64-1.9c.21-.07.36-.25.36-.48V3.5c0-.28-.22-.5-.5-.5zM15 19l-6-2.11V5l6 2.11V19z" />
+          </svg>
+          <span className="pl1">Posłuchaj</span>
+        </Link>
+        <footer
+          className="absolute bottom-0 right-0 left-0 f6 pa2 white"
+          style={{
+            background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8) )'
+          }}
+        >
+          <span className="fr">
+            built with ❤️️ by:
+            {' '}
+            <a
+              href="https://endziu.xyz"
+              className="link white hover-bg-white hover-black"
+            >
+              endziu.xyz
+            </a>
+          </span>
+        </footer>
+      </div>
     </div>
-  </div>
-</header>
+  </header>
+)
 
 export default Welcome
