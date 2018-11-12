9aba4337ee333451bf1d368b174a79766867c111
endziu <koper.andrzej@gmail.com>
2018-11-13 00:18:32 +0100

    pretty

--- shared/comps/Mapa.js
+++ shared/comps/Mapa.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react'
-import { Gmaps, Marker } from 'react-gmaps'
+import React, { Component } from "react"
+import { Gmaps, Marker } from "react-gmaps"
 
-const params = { v: '3', key: 'AIzaSyB7GQAjLtFuxLYjfSQq3PLX3o0mX6qT-CU', language: 'pl', region: 'PL' }
+const params = {
+  v: "3",
+  key: "AIzaSyB7GQAjLtFuxLYjfSQq3PLX3o0mX6qT-CU",
+  language: "pl",
+  region: "PL"
+}
 
 class SoundMap extends Component {
-
-  onMapCreated (map) {
+  onMapCreated(map) {
     map.setOptions({
       disableDefaultUI: false,
-      mapTypeId: 'satellite',
+      mapTypeId: "satellite",
       mapTypeControl: false,
       fullscreenControl: false,
       streetViewControl: true,
@@ -17,31 +21,36 @@ class SoundMap extends Component {
     })
   }
 
-  onMarkerClick (index) {
+  onMarkerClick(index) {
     this.props.onClick(index)
   }
 
+  shouldComponentUpdate() {
+    return true
+  }
 
-  render () {
+  render() {
     const current = this.props.currentSound
     return (
       <Gmaps
-        ref={'mapa'}
-        width={'100%'}
-        className='flex justify-end-ns w-100 vh-40 vh-100-ns fadeIn animated ph1 pt1 fadeIn animated'
+        ref={"mapa"}
+        width={"100%"}
+        className="flex justify-end-ns w-100 vh-40 vh-100-ns fadeIn animated ph1 pt1 fadeIn animated"
         lat={current.info.gps.lat}
         lng={current.info.gps.lng}
         params={params}
         onMapCreated={this.onMapCreated}
       >
         {this.props.sounds.map((sound, index) => {
-          return <Marker
-            animation={current.id === sound.id ? 1 : 0}
-            lat={sound.info.gps.lat}
-            lng={sound.info.gps.lng}
-            onClick={this.onMarkerClick.bind(this, index)}
-            key={index}
-          />
+          return (
+            <Marker
+              animation={current.id === sound.id ? 1 : 0}
+              lat={sound.info.gps.lat}
+              lng={sound.info.gps.lng}
+              onClick={this.onMarkerClick.bind(this, index)}
+              key={index}
+            />
+          )
         })}
       </Gmaps>
     )
