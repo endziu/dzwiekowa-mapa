66fb9f49ca7a961243438915273e7e86ea08d7a9
endziu <koper.andrzej@gmail.com>
2019-04-10 14:13:38 +0200

    change link to first sound

--- shared/comps/Welcome.js
+++ shared/comps/Welcome.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react"
+import { Link } from "react-router-dom"
 
-const urlBase = 'https://images.unsplash.com/photo-1470326032834-47d8a3054bb4'
+const urlBase = "https://images.unsplash.com/photo-1470326032834-47d8a3054bb4"
 const urlParams =
-  '?dpr=1&auto=compress,format&fit=crop&w=1920&h=1080&q=60&cs=tinysrgb&crop=&bg='
+  "?dpr=1&auto=compress,format&fit=crop&w=1920&h=1080&q=60&cs=tinysrgb&crop=&bg="
 const unsplashUrl = `${urlBase}${urlParams}`
 
 const Welcome = () => (
@@ -12,9 +12,9 @@ const Welcome = () => (
       className="bg-near-white h-100"
       style={{
         backgroundImage: `url(${unsplashUrl})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'left',
-        backgroundRepeat: 'no-repeat'
+        backgroundSize: "cover",
+        backgroundPosition: "left",
+        backgroundRepeat: "no-repeat"
       }}
     >
 
@@ -26,13 +26,13 @@ const Welcome = () => (
           kolekcja nagrań audiosfery miasta
         </h2>
         <Link
-          to={'/fontanna_rynek/map'}
+          to={"/151128-jarmark-bozonarodzeniowy-we-wroclawiu/map"}
           className="v-mid f5 no-underline black bg-white bg-animate hover-bg-black hover-white inline-flex items-center pa2 ba border-box mv3"
         >
           <svg
             className="w1"
             viewBox="0 0 24 24"
-            style={{ fill: 'currentcolor' }}
+            style={{ fill: "currentcolor" }}
           >
             <title>Map icon</title>
             <path d="M20.5 3l-.16.03L15 5.1 9 3 3.36 4.9c-.21.07-.36.25-.36.48V20.5c0 .28.22.5.5.5l.16-.03L9 18.9l6 2.1 5.64-1.9c.21-.07.36-.25.36-.48V3.5c0-.28-.22-.5-.5-.5zM15 19l-6-2.11V5l6 2.11V19z" />
@@ -42,12 +42,12 @@ const Welcome = () => (
         <footer
           className="absolute bottom-0 right-0 left-0 f6 pa2 white"
           style={{
-            background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8) )'
+            background: "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8) )"
           }}
         >
           <span className="fr">
             built with ❤️️ by:
-            {' '}
+            {" "}
             <a
               href="https://endziu.xyz"
               className="link white hover-bg-white hover-black"
